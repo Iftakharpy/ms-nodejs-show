d9c039305970c51557d7cd796d97e6f8a068fb93
Iftakhar Husan <iftakharn19@gmail.com>
2022-05-23 07:09:22 +0600

    Add endpoint to send package name and version

--- express-web-api/server.js
+++ express-web-api/server.js
@@ -2,16 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const packageDescription = require('../package.json')
 const serverPort = 5000;
+const apiRoot = '/api'
+
 
 const app = express();
+const router = express.Router()
+
+// add router to the app
+app.use(apiRoot, router)
+
 // configure express app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({ origin: /http:\/\/localhost/ }));
 app.options("*", cors());
 
-
+// Start express server
 app.listen(serverPort, () => {
   console.log(`Server is up on port ${serverPort}`);
 });
+
+
+// Add get endpoint
+router.get('/', (request, response)=>{
+  response.send(`${packageDescription.name} - v${packageDescription.version}`)
+})
