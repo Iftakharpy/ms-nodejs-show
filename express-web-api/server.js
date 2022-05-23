9bbc25f88fdfe1c9ffdbd90fa70be55cec37c911
Iftakhar Husan <iftakharn19@gmail.com>
2022-05-23 09:33:08 +0600

    Add uid and accounts creation and accounts list endpoints

--- express-web-api/server.js
+++ express-web-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const uuid = require('uuid')
 
 const packageDescription = require("../package.json");
 const serverPort = 5000;
@@ -27,19 +28,21 @@ const DB = {
 const app = express();
 const router = express.Router();
 
-// add router to the app
-app.use(apiRoot, router);
-
 // configure express app
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cors({ origin: /http:\/\/localhost/ }));
 app.options("*", cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// register router to the app
+app.use(apiRoot, router);
+
 
 // Start express server
 app.listen(serverPort, () => {
   console.log(`Server is up on port ${serverPort}`);
 });
+
 
 // Add api endpoints
 router.get("/", (request, response) => {
@@ -49,9 +52,46 @@ router.get("/", (request, response) => {
 router.get("/accounts/:user", (request, response) => {
   let user = request.params.user;
   let userAccount = DB[user];
-
+  
   if (userAccount === undefined) {
     return response.status(404).json({ error: "User account does not exist!" });
   }
   return response.json(userAccount);
 });
+
+router.get('/accounts', (request, response)=>{
+  response.send(DB)
+})
+
+router.post('/accounts', (request, response)=>{
+  let payload = request.body
+
+  // validate required values
+  if (!payload?.userName || !payload?.currency || !payload?.userId){
+    return response.status(400).send({error: 'userName and currency is required'})
+  }
+  
+  // check if account already exist
+  if (DB[payload?.userId]){
+    return response.status(400).send({error: 'Account already exists'})
+  }
+
+  // check balance
+  let balance = payload?.balance
+  if (balance && typeof(balance)!=='number'){
+    balance = parseFloat(balance)
+    if (isNaN(balance)){
+      return response.status(400).send({error: 'balance must be a number'})
+    }
+  }
+
+  let account = {
+    userName: payload.userName,
+    currency: payload.currency,
+    description: payload.description || `${payload.userName}'s account`,
+    balance: balance||0,
+    transactions: []
+  }
+  DB[payload.userId] = account
+  return response.json(account)
+})
